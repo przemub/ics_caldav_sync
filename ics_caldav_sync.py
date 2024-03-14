2dbd1814fe7a2733daf5f41fa9cdf1a8061b7a4e
Przemysław Buczkowski <prem@prem.moe>
2024-03-14 16:25:20 +0000

    Add DEBUG and SYNC_ALL options, improve error handling
    When debugging #3, I added some helpful options.
    When DEBUG is set, logging level is set to, you guessed it, DEBUG.
    When SYNC_ALL is set, all events are pulled, not just the future ones.
    In addition, more helpful messages are printed when a failure occurs.

--- ics_caldav_sync.py
+++ ics_caldav_sync.py
@@ -4,8 +4,14 @@ import time
 
 import arrow
 import caldav
+import caldav.lib.error
 import ics
 import requests
+import vobject.base
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class ICSToCalDAV:
@@ -35,6 +41,7 @@ class ICSToCalDAV:
         local_password: str,
         remote_username: str = "",
         remote_password: str = "",
+        sync_all: bool = False
     ):
         self.local_client = caldav.DAVClient(
             url=local_url,
@@ -51,14 +58,25 @@ class ICSToCalDAV:
             ).text
         )
 
+        self.sync_all = sync_all
+
     def _get_local_events_ids(self) -> set[int]:
         """
         This piece of crap:
         1) Gets from the local calendar all the events ocurring after now,
         2) Loads them to ics library so their UID can be pulled,
         3) Pulls all of the UIDs and returns them.
+
+        If sync_all is set, then all events will be pulled.
         """
-        local_events = self.local_calendar.date_search(arrow.utcnow())
+        if self.sync_all:
+            local_events = self.local_calendar.events()
+        else:
+            try:
+                local_events = self.local_calendar.search(start=arrow.utcnow())
+            except caldav.lib.error.ReportError:
+                logger.critical("Server failed when filtering events. Try SYNC_ALL=1 to do a full sync.")
+                raise
         local_events_ids = set(
             next(iter(ics.Calendar(e.data).events)).uid for e in local_events
         )
@@ -81,15 +99,21 @@ END:VCALENDAR
     def synchronise(self):
         """
         The main function which:
-        1) Pulls all the events from the remote calendar,
+        1) Pulls the events from the remote calendar,
         2) Saves them into the local calendar,
         3) Removes local events which are not in the remote any more.
+
+        If sync_all is set, all events will be pulled. Otherwise, only
+        the ones occuring after now will be.
         """
         for remote_event in self.remote_calendar.events:
-            if arrow.utcnow() > remote_event.end:
+            if not self.sync_all and arrow.utcnow() > remote_event.end:
                 continue
 
-            self.local_calendar.save_event(self._wrap(remote_event))
+            try:
+                self.local_calendar.save_event(self._wrap(remote_event))
+            except vobject.base.ValidateError:
+                logger.exception("Invalid event was downloaded from the remote. It will be skipped.")
             print("+", end="")
             sys.stdout.flush()
         print()
@@ -112,6 +136,9 @@ def getenv_or_raise(var):
 
 
 if __name__ == "__main__":
+    if os.getenv("DEBUG"):
+        logging.basicConfig(level=logging.DEBUG)
+
     settings = {
         "remote_url": getenv_or_raise("REMOTE_URL"),
         "local_url": getenv_or_raise("LOCAL_URL"),
@@ -120,6 +147,7 @@ if __name__ == "__main__":
         "local_password": getenv_or_raise("LOCAL_PASSWORD"),
         "remote_username": os.getenv("REMOTE_USERNAME", ""),
         "remote_password": os.getenv("REMOTE_PASWORD", ""),
+        "sync_all": bool(os.getenv("SYNC_ALL", None)),
     }
 
     sync_every = os.getenv("SYNC_EVERY", None)
